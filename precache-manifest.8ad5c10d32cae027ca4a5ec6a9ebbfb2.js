226d5ba3dc920245ebe8a07aac93dc724c6f07b1
Brian Mello <brianmmello@gmail.com>
2020-06-30 23:49:46 -0300

    Updates

---
+++ precache-manifest.8ad5c10d32cae027ca4a5ec6a9ebbfb2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e571df64475af2322b2ba04219ad7d5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86632eefbc9af06f123f",
+    "url": "/static/js/2.bdc93768.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bdc93768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7457a00460095a995a9",
+    "url": "/static/js/main.75582524.chunk.js"
+  },
+  {
+    "revision": "4a89d1e0d4d4f12f6cda",
+    "url": "/static/js/runtime-main.0a3577ac.js"
+  },
+  {
+    "revision": "41fbc0691d3e31263da44f25be2bf7de",
+    "url": "/static/media/ChuckNorris.41fbc069.jpg"
+  },
+  {
+    "revision": "6bc34076fc32f9a40a9c5fcc844fbcf7",
+    "url": "/static/media/animal.6bc34076.jpg"
+  },
+  {
+    "revision": "65fc8bbd3da51a2b0955f62f30badd6f",
+    "url": "/static/media/career.65fc8bbd.jpg"
+  },
+  {
+    "revision": "4900587309405135973efd845a452cc9",
+    "url": "/static/media/celebrity.49005873.jpg"
+  },
+  {
+    "revision": "45c0b8c528fa757b1684b631a0ad7086",
+    "url": "/static/media/dev.45c0b8c5.jpg"
+  },
+  {
+    "revision": "db3cb5445a65691793c9be807c8d9e71",
+    "url": "/static/media/explicit.db3cb544.png"
+  },
+  {
+    "revision": "3f138986fd57da7603b8954d3b791b90",
+    "url": "/static/media/fashion.3f138986.png"
+  },
+  {
+    "revision": "eb98c595995dd3982ac2c92fd798ddce",
+    "url": "/static/media/food.eb98c595.jpg"
+  },
+  {
+    "revision": "b4de1b82e64972c1d2042bdc27493006",
+    "url": "/static/media/history.b4de1b82.jpg"
+  },
+  {
+    "revision": "36a0ddd30aea36f0a4849b8a5f315292",
+    "url": "/static/media/money.36a0ddd3.jpg"
+  },
+  {
+    "revision": "b57d951b9e48b42c612e7500c30e56dc",
+    "url": "/static/media/movie.b57d951b.jpg"
+  },
+  {
+    "revision": "6bc54cedf40cc918184a453b0226d92d",
+    "url": "/static/media/music.6bc54ced.jpg"
+  },
+  {
+    "revision": "b9e3a1c8c5b7f43da652fc00f6e5b477",
+    "url": "/static/media/politic.b9e3a1c8.jpg"
+  },
+  {
+    "revision": "e75b106700807af289cd9f4a1b1f12d7",
+    "url": "/static/media/religion.e75b1067.png"
+  },
+  {
+    "revision": "ebf917dc409e9f9943f1b10700f23faa",
+    "url": "/static/media/science.ebf917dc.jpg"
+  },
+  {
+    "revision": "f5d31e267e83c62148a74589bd625602",
+    "url": "/static/media/sport.f5d31e26.jpg"
+  },
+  {
+    "revision": "03cfaa4ed428534f2a6e0a9faabdcd99",
+    "url": "/static/media/travel.03cfaa4e.jpg"
+  }
+]);
